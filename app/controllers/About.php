7696a5124b52caaf9a90feacdfe8d89b510f1ac4
andrisyahputra <andrisyahputra2209@gmail.com>
2023-06-09 09:59:33 +0700

    view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,22 @@
 <?php
-    class About {
-        public function index($nama = "Andri Syahputra", $pekerjaan = "Developer")
+    class About extends Controller{
+        public function index($nama = "Andri Syahputra", $pekerjaan = "Developer", $umur = "23")
         {
-            echo "Helo nama saya $nama, saya adalah seorang $pekerjaan";
+            // echo "Helo nama saya $nama, saya adalah seorang $pekerjaan";
+            $data['judul'] = "About Me";
+            $data['nama'] = $nama;
+            $data['pekerjaan'] = $pekerjaan;
+            $data['umur'] = $umur;
+            $this->view('templates/header', $data);
+            $this->view('about/index', $data);
+            $this->view('templates/footer');
         }
         public function page()
         {
-            echo "About/page";
+            $data['judul'] = 'Pages';
+            $this->view('templates/header', $data);
+            $this->view('about/page');
+            $this->view('templates/footer');
         }
     }
 ?>
---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+    <h1>About Me</h1>
+    <ul>
+        <li>Nama : <?= $data['nama'] ?></li>
+        <li>Pekerjaan : <?= $data['pekerjaan'] ?></li>
+        <li>Umur : <?= $data['umur'] ?></li>
+    </ul>
